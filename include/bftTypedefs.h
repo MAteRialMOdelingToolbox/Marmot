54b7dc2f58c59fc597883126c16c9d74ce8dc344
MatthiasN <matthias.neuner@uibk.ac.at>
2016-02-19 13:32:52 +0100

    removed using keyword

--- include/bftTypedefs.h
+++ include/bftTypedefs.h
@@ -11,9 +11,4 @@ namespace bft{
     typedef Matrix<double,6,1> Vector6;
     typedef Map<Vector6> mVector6;
     typedef Map<const Vector6> mConstVector6;
-
-    template <std::size_t size> 
-        using YieldSurfFlagArr = Array<bool, 1, size>;
-    template <std::size_t size>
-        using YieldSurfResArr = Array<double, 1, size>;
 };
