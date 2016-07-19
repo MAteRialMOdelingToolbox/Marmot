c8c37bfde3f801af7d6e752fc06820a65ad32297
Magdalena Schreter <magdalena.schreter@uibk.ac.at>
2016-07-19 14:53:17 +0200

    again derivatives for damage proofed; in triaxial tests analytical derivative of damage doesn't comply with central differences

--- buildx64.bat
+++
@@ -1,7 +0,0 @@
-::This is a batch file for creating a Win64 Visual Studio Project
-cmake . -G "Visual Studio 11 Win64"
-call "C:\Program Files (x86)\Microsoft Visual Studio 11.0\VC\vcvarsall.bat" x86_amd64
-call "C:\Program Files (x86)\Intel\Composer XE 2013\bin\ifortvars.bat" intel64 vs2012
-set mypath=%cd%
-cd myPath
-MSBuild.exe "bftMechanics.sln"
