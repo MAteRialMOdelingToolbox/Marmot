ea4215639c32b6b4b00984b3f70c2de01136de6e
MatthiasN <matthias.neuner@uibk.ac.at>
2016-12-19 17:11:44 +0100

    added convencience functions for nonlocal umats

--- include/bftAbaqusUtility.h
+++ include/bftAbaqusUtility.h
@@ -8,6 +8,13 @@
 namespace bft{
     void backToAbaqus(const Matrix6& jacobian, Map<MatrixXd>& ABQJacobian, const Vector6& stress, Map<VectorXd>& ABQStress, int nTensor);
     void backToAbaqusPlaneStress(const Matrix6& jacobian, Map<MatrixXd>& ABQJacobian, const Vector6& stress, Map<VectorXd>& ABQStress);
+    void backToAbaqusNonLocal(  const Matrix6& dStressdStrain,              Ref<MatrixXd>  ABQdStressDStrain, 
+                                const Vector6& stress,                      Ref<VectorXd>  ABQStress,
+                                double intParameterLocal,                   double&         ABQParameterLocal,
+                                const Vector6& dStressDIntParamNonLocal,    Ref<VectorXd>   ABQDStressDIntParamNonLocal,
+                                const Vector6& dIntParamLocalDStrain,       Ref<VectorXd>   ABQDIntParameterLocalDStrain,
+                                double nonLocalRadius,                      double&         ABQNonLocalRadius,
+                                int nTensor);
     void discardIncrementAndBackToAbaqus(double& pNewDT, double value, const std::string& message);
 
 }
