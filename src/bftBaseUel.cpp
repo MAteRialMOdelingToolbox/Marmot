e4743b0da7496106af593f45d7d7e2de818dd70e
matthias <matthias.neuner@uibk.ac.at>
2017-04-02 17:31:04 +0200

    added new func ptr type simpleUelWithUmat

---
+++ src/bftBaseUel.cpp
@@ -0,0 +1,19 @@
+#include "bftUel.h"
+
+//BftUel:: BftUel(                         const double* coordinates,
+                                            //double* stateVarsTotal,
+                                            //int nStateVarsTotal,
+                                            //const double* propertiesElement,
+                                            //int nPropertiesElement,
+                                            //int noEl,
+                                            //const bft::pUmatType umat,
+                                            //int nStateVarsUmat, 
+                                            //const double* propertiesUmat,
+                                            //int nPropertiesUmat)
+//{
+//}
+
+//BftUel::~BftUel()
+//{
+//}
+
