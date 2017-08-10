3f6d10a6b334985ac7b9490bfbc15b5f02f3908a
c8441161 <andreas.brugger@uibk.ac.at>
2017-08-10 09:51:57 +0200

    tougher restriction in exponential function to avoid overflow

--- src/bftFunctions.cpp
+++ src/bftFunctions.cpp
@@ -6,15 +6,15 @@ namespace bft{
     {
         // return linear interpolation of polynom y at given coordinates (x0, y0) and (x1, y1) at point x 
         double linearInterpolation(double x, double x0, double x1, double y0, double y1)
-        { return y0 + (x - x0) * (y1 -y0)/(x1 - x0);}   
+        { return y0 + (x - x0) * (y1 -y0)/(x1 - x0);}
 
+        // bounded version of std::exp
         double exp(double x)
         {
-            // bounded version of std::exp
-            if(x <= -708.4)
+            if(x <= -64)    // underflow if arg < -708.4 (type double)
                 return 0.0;
-            if(x >= 709.8)
-                return std::exp(709.8);
+            if(x >= 64)    // overflow if arg > 709.8 (type double), leave ample margin (e.g. for squaring)
+                return std::exp(64);
             return std::exp(x);
         }
 
