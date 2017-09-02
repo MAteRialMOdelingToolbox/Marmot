3f66e2587e69bb319c6d442ec5997a087353cd49
matthias <matthias.neuner@uibk.ac.at>
2017-09-02 09:05:17 +0200

    working prototype for 4Noded elements / Modleon

---
+++ include/bftMaterialHypoElastic.h
@@ -0,0 +1,31 @@
+#pragma once 
+#include "bftMaterial.h"
+
+class BftMaterialHypoElastic : public BftMaterial{
+
+    public: 
+
+        using BftMaterial::BftMaterial;
+
+        double characteristicElementLength;
+        void setCharacteristicElementLength(double length) { characteristicElementLength = length; }
+
+        // Abstract methods
+        virtual void computeStress(
+                double *stress, 
+                double* dStressDDStrain,  
+                const double *strainOld,
+                const double *dStrain,
+                const double* timeOld,
+                const double  dT,
+                double& pNewDT) = 0;
+
+        virtual void computePlaneStress(
+                double *stress, 
+                double* dStressDDStrain,  
+                const double *strainOld,
+                double *dStrain,
+                const double* timeOld,
+                const double  dT,
+                double& pNewDT);
+};
