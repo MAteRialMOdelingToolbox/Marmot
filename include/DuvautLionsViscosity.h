f98fe2e928fd14688d13570294d0c01ada0609e2
Matthias <matthias.neuner@uibk.ac.at>
2018-10-22 08:12:44 +0200

    complete cleanup

--- include/DuvautLionsViscosity.h
+++ include/DuvautLionsViscosity.h
@@ -1,49 +1,46 @@
 #pragma once
 #include "bftTypedefs.h"
 
-namespace bft{
-    template <int nMatTangentSize>
-    class DuvautLionsViscosity
-    {
-        private:
-            const double viscosity;
-        public:
-            typedef  Matrix<double, nMatTangentSize, nMatTangentSize> TangentSizedMatrix;
+namespace bft {
+    template <int nMatTangentSize> class DuvautLionsViscosity {
+      private:
+        const double viscosity;
 
-            DuvautLionsViscosity(double viscosity);
-            double applyViscosityOnStateVar(double stateVarTrial, double StateVarInf, double dT);
-            Vector6 applyViscosityOnStress(const Vector6& trialStress, const Vector6& stressInf, double dT);
-            TangentSizedMatrix applyViscosityOnMatTangent(
-                    const TangentSizedMatrix& matTangentInv, 
-                    double dT); 
+      public:
+        typedef Matrix<double, nMatTangentSize, nMatTangentSize> TangentSizedMatrix;
+
+        DuvautLionsViscosity( double viscosity );
+        double  applyViscosityOnStateVar( double stateVarTrial, double StateVarInf, double dT );
+        Vector6 applyViscosityOnStress( const Vector6& trialStress,
+                                        const Vector6& stressInf,
+                                        double         dT );
+        TangentSizedMatrix applyViscosityOnMatTangent( const TangentSizedMatrix& matTangentInv,
+                                                       double                    dT );
     };
-}
+} // namespace bft
 
-namespace bft
-{
+namespace bft {
     template <int s>
-    DuvautLionsViscosity<s>::DuvautLionsViscosity(double viscosity):
-            viscosity(viscosity)
-    {
+    DuvautLionsViscosity<s>::DuvautLionsViscosity( double viscosity ) : viscosity( viscosity ) {}
 
+    template <int s>
+    double DuvautLionsViscosity<s>::applyViscosityOnStateVar( double stateVarTrial,
+                                                              double StateVarInf,
+                                                              double dT ) {
+        return ( stateVarTrial + ( dT / viscosity ) * StateVarInf ) / ( dT / viscosity + 1 );
     }
 
     template <int s>
-    double DuvautLionsViscosity<s>::applyViscosityOnStateVar(double stateVarTrial, double StateVarInf,  double dT)
-    {
-            return (stateVarTrial + (dT/viscosity) * StateVarInf)/(dT/viscosity + 1); 
+    Vector6 DuvautLionsViscosity<s>::applyViscosityOnStress( const Vector6& trialStress,
+                                                             const Vector6& stressInf,
+                                                             double         dT ) {
+        return ( trialStress + ( dT / viscosity ) * stressInf ) / ( dT / viscosity + 1 );
     }
 
     template <int s>
-    Vector6 DuvautLionsViscosity<s>::applyViscosityOnStress(const Vector6& trialStress, const Vector6& stressInf, double dT)
-    {
-            return (trialStress + (dT/viscosity) * stressInf)/(dT/viscosity + 1); 
+    typename DuvautLionsViscosity<s>::TangentSizedMatrix DuvautLionsViscosity<
+        s>::applyViscosityOnMatTangent( const TangentSizedMatrix& matTangentInv, double dT ) {
+        return ( 1 / ( 1 + dT / viscosity ) ) *
+               ( TangentSizedMatrix::Identity() + dT / viscosity * matTangentInv );
     }
-            
-    template <int s>
-    typename DuvautLionsViscosity<s>::TangentSizedMatrix 
-        DuvautLionsViscosity<s>::applyViscosityOnMatTangent(const TangentSizedMatrix& matTangentInv, double dT)
-    {
-            return (1/(1+dT/viscosity)) * (TangentSizedMatrix::Identity() + dT/viscosity * matTangentInv);
-    }
-}
+} // namespace bft
