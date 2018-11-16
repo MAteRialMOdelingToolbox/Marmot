b0a880b13558e4b21c9abfefc1e9d6ea68a3f5ef
Matthias <matthias.neuner@uibk.ac.at>
2018-11-16 14:46:55 +0100

    bugfix

--- include/uelDisplacement.h
+++ include/uelDisplacement.h
@@ -98,7 +98,10 @@ class UelDisplacement : public BftUel, public BftGeometryElement<nDim, nNodes> {
                                  const double*                time,
                                  double                       dT );
 
-    void computeBodyForce( double* P, const double* load, const double* time, double dT );
+    void computeBodyForce( double*                      P,
+                                                            const double*                load,
+                                                            const double*                time,
+                                                            double                       dT );
 
     void computeYourself( const double* QTotal,
                           const double* dQ,
@@ -428,12 +431,17 @@ void UelDisplacement<nDim, nNodes>::computeDistributedLoad( BftUel::DistributedL
     }
 }
 
-template <int nDim, int nNodes>
-void UelDisplacement<nDim, nNodes>::computeBodyForce( double* P_, const double* load, const double* time, double dT )
-{
-    Map<RhsSized>                            Pe( P_ );
-    const Map<const Matrix<double, nDim, 1>> f( load );
 
-    for ( const auto& gpt : gaussPts )
-        Pe = this->NB( this->N( gpt.xi ) ).transpose() * f * gpt.geometry->intVol;
+template <int nDim, int nNodes>
+void UelDisplacement<nDim, nNodes>::computeBodyForce( double*                      P_,
+                                                            const double*                load,
+                                                            const double*                time,
+                                                            double                       dT )
+{
+    Map<RhsSized>       Pe( P_ );
+    const Map< const Matrix<double, nDim, 1> >             f( load );
+
+    for ( const auto& gpt : gaussPts) 
+        Pe += this->NB( this->N ( gpt.xi ) ).transpose() * f * gpt.geometry->intVol;
 }
+
