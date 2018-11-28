f130676eedea57fbd994677a5b4fc3e5df6d2be1
Matthias <matthias.neuner@uibk.ac.at>
2018-11-28 17:20:45 +0100

    refactoring_progress

--- include/LinearElastic.h
+++ include/LinearElastic.h
@@ -1,34 +1,27 @@
 #pragma once
 #include "bftMaterialHypoElastic.h"
-#include <string>
 #include <iostream>
+#include <string>
 
-class LinearElastic: public BftMaterialHypoElastic
-{ 
-        public: 
+class LinearElastic : public BftMaterialHypoElastic {
+  public:
+    using BftMaterialHypoElastic::BftMaterialHypoElastic;
 
-        using BftMaterialHypoElastic::BftMaterialHypoElastic;
+    void computeStress( double*       stress,
+                        double*       dStressDDStrain,
+                        const double* strainOld,
+                        const double* dStrain,
+                        const double* timeOld,
+                        const double  dT,
+                        double&       pNewDT );
 
-        void computeStress(
-                double *stress, 
-                double* dStressDDStrain,  
-                const double *strainOld,
-                const double *dStrain,
-                const double* timeOld,
-                const double  dT,
-                double& pNewDT);
+    double* getPermanentResultPointer( const std::string& result, int& resultLength ) { return nullptr; }
 
-        double* getPermanentResultPointer(const std::string& result, int& resultLength){
-        return nullptr;}
+    int getNumberOfRequiredStateVars() { return 2; }
 
-        int getNumberOfRequiredStateVars() {
-            return 2;
-        }
-
-        void assignStateVars(double *stateVars, int nStateVars) { 
-            this->stateVars = stateVars;
-            this->nStateVars = nStateVars;
-        };
-
+    void assignStateVars( double* stateVars, int nStateVars )
+    {
+        this->stateVars  = stateVars;
+        this->nStateVars = nStateVars;
+    };
 };
-
--- src/LinearElastic.cpp
+++ src/LinearElastic.cpp
@@ -1,41 +1,42 @@
 #include "LinearElastic.h"
-#include "bftTypedefs.h"
-#include "bftFunctions.h"
 #include "bftAbaqusUtility.h"
+#include "bftFunctions.h"
+#include "bftTypedefs.h"
 #include "bftVoigt.h"
 
 using namespace bft;
+using namespace Eigen;
 
-void LinearElastic::computeStress(
-        double *stress, 
-        double* dStressDDStrain,  
-        const double *strainOld,
-        const double *dStrain,
-        const double* timeOld,
-        const double  dT,
-        double& pNewDT)
-{		
-    //material properties
-    const double& E  =                      materialProperties[0];
-    const double& nu =                      materialProperties[1];
+void LinearElastic::computeStress( double*       stress,
+                                   double*       dStressDDStrain,
+                                   const double* strainOld,
+                                   const double* dStrain,
+                                   const double* timeOld,
+                                   const double  dT,
+                                   double&       pNewDT )
+{
+    // material properties
+    const double& E  = materialProperties[0];
+    const double& nu = materialProperties[1];
 
     const int nTensor = 6;
 
-    Matrix6 Cel = 							mechanics::Cel(E,nu);
+    Matrix6 Cel = mechanics::Cel( E, nu );
 
-    Map<VectorXd>                           abqNomStress(stress, nTensor);
-    Map<const VectorXd>                     abqDE(dStrain, nTensor);
-    Map<MatrixXd>                           abqC(dStressDDStrain, nTensor, nTensor);   
+    Map<VectorXd>       abqNomStress( stress, nTensor );
+    Map<const VectorXd> abqDE( dStrain, nTensor );
+    Map<MatrixXd>       abqC( dStressDDStrain, nTensor, nTensor );
 
-    bft::Vector6 oldStress =                Vector6::Zero(); oldStress.head(nTensor)= abqNomStress;
-    bft::Vector6 dE =                       Vector6::Zero(); dE.head(nTensor)       = abqDE;
+    bft::Vector6 oldStress    = Vector6::Zero();
+    oldStress.head( nTensor ) = abqNomStress;
+    bft::Vector6 dE           = Vector6::Zero();
+    dE.head( nTensor )        = abqDE;
 
     // Zero strain  increment check
-    if((dE.array() == 0).all())
-        return backToAbaqus(Cel, abqC, oldStress, abqNomStress, nTensor);               
+    if ( ( dE.array() == 0 ).all() )
+        return backToAbaqus( Cel, abqC, oldStress, abqNomStress, nTensor );
 
-    Vector6 newStress = oldStress + Cel * dE;                                                                         
+    Vector6 newStress = oldStress + Cel * dE;
 
-    return backToAbaqus(Cel, abqC,newStress, abqNomStress, nTensor);
+    return backToAbaqus( Cel, abqC, newStress, abqNomStress, nTensor );
 }
-
