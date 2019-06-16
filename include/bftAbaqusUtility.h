25abc02f7fd57534e2ee7ec9ef62faccb8b17661
Matthias <matthias.neuner@uibk.ac.at>
2019-06-16 20:31:55 +0200

    intermediate safe

--- include/bftAbaqusUtility.h
+++ include/bftAbaqusUtility.h
@@ -6,20 +6,20 @@
  * */
 
 namespace bft {
-    [[deprecated]]
+    [[deprecated("BftMaterial input is always Voigt6 - sized -> these aux. functions are not necessary anymore")]]
     void backToAbaqus( const Matrix6&               jacobian,
                        Eigen::Map<Eigen::MatrixXd>& ABQJacobian,
                        const bft::Vector6&          stress,
                        Eigen::Map<Eigen::VectorXd>& ABQStress,
                        int                          nTensor );
 
-    [[deprecated]]
+    [[deprecated("BftMaterial input is always Voigt6 - sized -> these aux. functions are not necessary anymore")]]
     void backToAbaqusPlaneStress( const Matrix6&               jacobian,
                                   Eigen::Map<Eigen::MatrixXd>& ABQJacobian,
                                   const bft::Vector6&          stress,
                                   Eigen::Map<Eigen::VectorXd>& ABQStress );
 
-    [[deprecated]]
+    [[deprecated("BftMaterial input is always Voigt6 - sized -> these aux. functions are not necessary anymore")]]
     void backToAbaqusNonLocal( const Matrix6&              dStressdStrain,
                                Eigen::Ref<Eigen::MatrixXd> ABQdStressDStrain,
                                const bft::Vector6&         stress,
