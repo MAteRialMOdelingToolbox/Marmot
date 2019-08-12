9bb1d16deeb21345ad9ba066924878c23d175aff
Magdalena <magdalena.schreter@uibk.ac.at>
2019-08-12 17:27:06 +0200

    bftMath: numeric integration methods -- function arguments modified

--- include/bftMath.h
+++ include/bftMath.h
@@ -27,14 +27,14 @@ namespace bft {
             return positivePart;
         }
 
-        template <typename functionType, typename yType, typename time, typename... Args>
-        yType explicitEuler( yType yN, time dt, functionType fRate, Args&&... fRateArgs )
+        template <typename functionType, typename yType, typename... Args>
+        yType explicitEuler( yType yN, const double dt, functionType fRate, Args&&... fRateArgs )
         {
             return yN + fRate( yN, fRateArgs... ) * dt;
         }
 
-        template <int ySize, typename functionType, typename yType, typename time, typename... Args>
-        yType semiImplicitEuler( yType yN, time dt, functionType fRate, Args&&... fRateArgs )
+        template <int ySize, typename functionType, typename... Args>
+        Eigen::Matrix<double, ySize, 1> semiImplicitEuler( Eigen::Matrix<double, ySize, 1> yN, const double dt, functionType fRate, Args&&... fRateArgs )
         {
             Eigen::Matrix<double, ySize, ySize> fS = Eigen::Matrix<double, ySize, ySize>::Zero();
             Eigen::Matrix<double, ySize, ySize> Iy = Eigen::Matrix<double, ySize, ySize>::Identity();
@@ -67,17 +67,17 @@ namespace bft {
             for ( size_t i = 0; i < nCols; i++ ) {
                 double volatile h = std::max( 1.0, std::abs( X( i ) ) ) * Constants::cubicRootEps();
                 leftX             = X;
-                leftX( i ) -= h;
-                rightX = X;
-                rightX( i ) += h;
+                leftX( i )       -= h;
+                rightX            = X;
+                rightX( i )      += h;
                 dXdY.col( i ) = 1. / ( 2. * h ) * ( f( rightX ) - f( leftX ) );
             }
 
             return dXdY;
         }
 
-        template <typename functionType, typename yType, typename time, typename... Args>
-        yType explicitEulerRichardson( yType yN, time dt, functionType fRate, Args&&... fRateArgs )
+        template <typename functionType, typename yType, typename... Args>
+        yType explicitEulerRichardson( yType yN, const double dt, functionType fRate, Args&&... fRateArgs )
         {
             yType fN = fRate( yN, fRateArgs... );
             yType u  = yN + fN * dt;
