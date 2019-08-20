20ae5ff8c26f51b31fe22cedf3e962a9c5c69a59
Matthias <matthias.neuner@uibk.ac.at>
2019-08-20 14:16:34 +0200

    working

--- include/bftMaterialMechanical.h
+++ include/bftMaterialMechanical.h
@@ -3,40 +3,52 @@
 
 class BftMaterialMechanical : public BftMaterial {
 
-    // Abstract basic class for Elastic materials.
-    // 'Elastic' is meant in the 'most general sense', i.e., any material which describes a mechanical (cauchy)
-    // stress - deformation relationship (e.g, elastic, elasto-plastic, visco-elastic materials)
-    //
-    // σ = f (σ, dxdX, t, .. ),
-    //
-    // formulated incrementally as σ_np = f (σ_n, dxdX_n, dxdX_np, Δt, t_n, .. )
-    //
-    // Algorithmic tangent: dσdε = d σ d [ sym( ( inv(F_n) * F_np) - I) ] (Abaqus compatible)
+    /*
+       Abstract basic class for Mechanical materials.
+       'Mechanical' is meant in the 'most general sense', i.e., any material which describes a mechanical (cauchy)
+       stress - deformation relationship (e.g, hyperelastic, hypoelastic, elasto-plastic, visco-elastic materials)
 
-  public:
-    using BftMaterial::BftMaterial;
+       σ = f (σ, dxdX, t, .. ),
 
-    virtual void computeStress( double*       stress,
-                                double*       dStressDDStrain,
-                                const double* FOld,
-                                const double* FNew,
-                                const double* timeOld,
-                                const double  dT,
-                                double&       pNewDT ) = 0;
+       formulated incrementally as σ_np = f (σ_n, dxdX_n, dxdX_np, Δt, t_n, .. )
 
-    virtual void computePlaneStress( double*       stress,
-                                     double*       dStressDDStrain,
-                                     const double* FOld,
-                                     double*       FNew,
-                                     const double* timeOld,
-                                     const double  dT,
-                                     double&       pNewDT );
+       Algorithmic tangent: dσdF = d σ_np d (dxdX_np)
 
-    virtual void computeUniaxialStress( double*       stress,
-                                        double*       dStressDDStrain,
-                                        const double* FOld,
-                                        double*       FNew,
-                                        const double* timeOld,
-                                        const double  dT,
-                                        double&       pNewDT );
+        Format:
+
+        / d σ_11 d F_00,   d σ_11 d F_10,   d σ_11 d F_20,   d σ_11 d F_01, \
+        | d σ_22 d F_00,                                                    |
+        | d σ_33 d F_00,                                                    |
+        | ...                                                               |
+        | ...                                                               |
+        \ ...                                                               /
+    */
+
+    public:
+
+        using BftMaterial::BftMaterial;
+
+        virtual void computeStress( double*       stress,
+                double*       dStressDDFNew,
+                const double* FOld,
+                const double* FNew,
+                const double* timeOld,
+                const double  dT,
+                double&       pNewDT ) = 0;
+
+        virtual void computePlaneStress( double*       stress,
+                double*       dStressDDFNew,
+                const double* FOld,
+                double*       FNew,
+                const double* timeOld,
+                const double  dT,
+                double&       pNewDT );
+
+        virtual void computeUniaxialStress( double*       stress,
+                double*       dStressDDFNew,
+                const double* FOld,
+                double*       FNew,
+                const double* timeOld,
+                const double  dT,
+                double&       pNewDT );
 };
