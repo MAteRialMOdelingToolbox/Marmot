22741c2cdd3daf65ec55b10a346ca25d398c8fd1
Matthias <matthias.neuner@uibk.ac.at>
2019-08-29 08:08:39 +0200

    implemented external HughesWinger module

--- src/bftMaterialHypoElasticNonLocal.cpp
+++ src/bftMaterialHypoElasticNonLocal.cpp
@@ -3,6 +3,7 @@
 #include "bftVoigt.h"
 #include "bftTensor.h"
 #include "bftKinematics.h"
+#include "HughesWinget.h"
 
 using namespace Eigen;
 
@@ -28,75 +29,85 @@ void BftMaterialHypoElasticNonLocal::computeStress( double*       stress_,
     // strain increment is computed at midstep
 
     using namespace bft;
-    using namespace bft::TensorUtility;
-    using namespace kinematics::velocityGradient;
+    //using namespace bft::TensorUtility;
+    //using namespace kinematics::velocityGradient;
 
     const Map<const Matrix3d> FNew( FNew_ );
     const Map<const Matrix3d> FOld( FOld_ );
     bft::mVector6             stress( stress_ );
-    Map<Matrix3d>             dKLocal_dF(dK_localDDeformationGradient_);
 
-    Matrix3d FMidStep = 0.5 * ( FNew + FOld );
+    //Matrix3d FMidStep = 0.5 * ( FNew + FOld );
 
-    Matrix3d l = ( FNew - FOld ) * FMidStep.inverse(); // actually l * dT
+    //Matrix3d l = ( FNew - FOld ) * FMidStep.inverse(); // actually l * dT
 
-    Matrix3d dEps_  = 0.5 * ( l + l.transpose() ); // actually d * dT
-    Matrix3d dOmega = 0.5 * ( l - l.transpose() ); // actually omega * dT
+    //Matrix3d dEps_  = 0.5 * ( l + l.transpose() ); // actually d * dT
+    //Matrix3d dOmega = 0.5 * ( l - l.transpose() ); // actually omega * dT
 
-    Matrix3d dR = ( Matrix3d::Identity() - 0.5 * dOmega ).inverse() * ( Matrix3d::Identity() + 0.5 * dOmega );
+    //Matrix3d dR = ( Matrix3d::Identity() - 0.5 * dOmega ).inverse() * ( Matrix3d::Identity() + 0.5 * dOmega );
 
-    stress = Vgt::stressToVoigt( dR * Vgt::voigtToStress( stress ) * dR.transpose() );
+    //stress = Vgt::stressToVoigt( dR * Vgt::voigtToStress( stress ) * dR.transpose() );
 
     Matrix6 CJaumann                = Matrix6::Zero();
-    Matrix6 dK_LocalDStretchingRate = Matrix6::Zero();
+    Vector6 dK_LocalDStretchingRate = Vector6::Zero();
 
-    Vector6 dEps = Vgt::VoigtFromStrainMatrix( dEps_ );
+    //Vector6 dEps = Vgt::VoigtFromStrainMatrix( dEps_ );
+    HughesWinget hughesWingetIntegrator( FOld, FNew, HughesWinget::Formulation::AbaqusLike );
+
+    auto dEps = hughesWingetIntegrator.getStrainIncrement();
+    stress    = hughesWingetIntegrator.rotateTensor( stress );
+
     computeStress( stress.data(), K_local, nonLocalRadius, CJaumann.data(), dK_LocalDStretchingRate.data(), 
                    dStressDK, dEps.data(), KOld, dK, timeOld, dT, pNewDT );
 
-
-    Tensor633d                          dS_dl;
-    Tensor633d                          dStressRotational_dl;
-    Tensor633d                          dStressJaumann_dl;
     TensorMap<Eigen::Tensor<double, 3>> dS_dF( dStressDDDeformationGradient_, 6, 3, 3 );
+    Map<Matrix3d>             dKLocal_dF(dK_localDDeformationGradient_);
 
-    auto stressNew = Vgt::StressMatrixFromVoigt<3>( stress );
+    Matrix3d FInv = FNew.inverse();
+    dS_dF = hughesWingetIntegrator.compute_dS_dF( stress, FInv, CJaumann );
+    dKLocal_dF = hughesWingetIntegrator.compute_dScalar_dF ( FInv, dK_LocalDStretchingRate);
 
-    dStressRotational_dl.setZero();
-    for ( int ij = 0; ij < 6; ij++ ) {
-        auto [i, j] = IndexNotation::fromVoigt<3>( ij );
-        for ( int k = 0; k < 3; k++ )
-            for ( int l = 0; l < 3; l++ )
-                for ( int m = 0; m < 3; m++ )
-                    dStressRotational_dl( ij, k, l ) += dOmega_dVelocityGradient( i, m, k, l ) * stressNew( m, j ) +
-                                                        dOmega_dVelocityGradient( j, m, k, l ) * stressNew( i, m );
-    }
+    //Tensor633d                          dS_dl;
+    //Tensor633d                          dStressRotational_dl;
+    //Tensor633d                          dStressJaumann_dl;
+    //TensorMap<Eigen::Tensor<double, 3>> dS_dF( dStressDDDeformationGradient_, 6, 3, 3 );
 
-    dStressJaumann_dl.setZero();
-    for ( int ij = 0; ij < 6; ij++ )
-        for ( int k = 0; k < 3; k++ )
-            for ( int l = 0; l < 3; l++ )
-                for ( int mn = 0; mn < 6; mn++ )
-                    dStressJaumann_dl( ij, k, l ) += CJaumann( ij, mn ) * dStretchingRate_dVelocityGradient( mn, k, l );
+    //auto stressNew = Vgt::StressMatrixFromVoigt<3>( stress );
 
-    dS_dl     = dStressJaumann_dl + dStressRotational_dl;
-    auto FInv = FNew.inverse();
+    //dStressRotational_dl.setZero();
+    //for ( int ij = 0; ij < 6; ij++ ) {
+        //auto [i, j] = IndexNotation::fromVoigt<3>( ij );
+        //for ( int k = 0; k < 3; k++ )
+            //for ( int l = 0; l < 3; l++ )
+                //for ( int m = 0; m < 3; m++ )
+                    //dStressRotational_dl( ij, k, l ) += dOmega_dVelocityGradient( i, m, k, l ) * stressNew( m, j ) +
+                                                        //dOmega_dVelocityGradient( j, m, k, l ) * stressNew( i, m );
+    //}
 
-    dS_dF.setZero();
-    for ( int ij = 0; ij < 6; ij++ )
-        for ( int k = 0; k < 3; k++ )
-            for ( int l = 0; l < 3; l++ )
-                for ( int m = 0; m < 3; m++ )
-                    dS_dF( ij, k, l ) += dS_dl( ij, k, m ) * FInv( l, m );
+    //dStressJaumann_dl.setZero();
+    //for ( int ij = 0; ij < 6; ij++ )
+        //for ( int k = 0; k < 3; k++ )
+            //for ( int l = 0; l < 3; l++ )
+                //for ( int mn = 0; mn < 6; mn++ )
+                    //dStressJaumann_dl( ij, k, l ) += CJaumann( ij, mn ) * dStretchingRate_dVelocityGradient( mn, k, l );
+
+    //dS_dl     = dStressJaumann_dl + dStressRotational_dl;
+    //auto FInv = FNew.inverse();
+
+    //dS_dF.setZero();
+    //for ( int ij = 0; ij < 6; ij++ )
+        //for ( int k = 0; k < 3; k++ )
+            //for ( int l = 0; l < 3; l++ )
+                //for ( int m = 0; m < 3; m++ )
+                    //dS_dF( ij, k, l ) += dS_dl( ij, k, m ) * FInv( l, m );
 
     
-    Matrix3d dK_Local_dl = Matrix3d::Zero();
-    for ( int k = 0; k < 3; k++ )
-        for ( int l = 0; l < 3; l++ )
-            for ( int ij = 0; ij < 6; ij++ )
-                dK_Local_dl( k, l ) += dK_LocalDStretchingRate(ij) * dStretchingRate_dVelocityGradient(ij, k, l);
+    //Matrix3d dK_Local_dl = Matrix3d::Zero();
+    //for ( int k = 0; k < 3; k++ )
+        //for ( int l = 0; l < 3; l++ )
+            //for ( int ij = 0; ij < 6; ij++ )
+                //dK_Local_dl( k, l ) += dK_LocalDStretchingRate(ij) * dStretchingRate_dVelocityGradient(ij, k, l);
     
-    dKLocal_dF = dK_Local_dl * FInv;
+    //dKLocal_dF = dK_Local_dl * FInv;
 }
 
 
