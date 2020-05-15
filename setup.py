7305e72f4f048a9850f5e55e07c26c3cb42a8bf0
Magdalena Schreter <magdalena.schreter@uibk.ac.at>
2020-05-15 16:18:29 +0200

    clean up

--- setup.py
+++ setup.py
@@ -6,11 +6,66 @@ Created on Fri May 15 11:47:51 2020
 @author: magdalena
 """
 
+
 import os
 import shutil
 from distutils.util import strtobool
 
 cwd = os.getcwd()
+defaultUser = 'afbDevelopers'
+
+lookUpMaterialCategories = {  "basic": "1",
+                              "concrete": "2",
+                              "shotcrete": "3",
+                              "soil": "4",
+                              "rock": "5",
+                              }
+
+dictMaterials = { # add material: 
+                  # name: ("basic/concrete/rock/shotcrete", "afbDevelopers/c8441141/c8441146")
+                "linearElastic":         ("basic", "afbDevelopers"),
+                "DruckerPrager":         ("basic", "c8441141"),
+                "MohrCoulomb":           ("basic", "c8441146"),
+                "CosseratLinearElastic": ("cosserat", "c8441141"),
+                "ModLeon":               ("concrete", "c8441141"),
+                "CDP":                   ("concrete", "c8441141"),
+                "CDPM2":                 ("concrete", "c8441141"),
+                "ModLeonNonLocal":       ("concrete", "c8441141"),      
+                "ShotLeonV2":            ("concrete", "c8441141"),        
+                "SolidificationCreep":   ("concrete", "c8441141"), 
+                "HoekBrown":             ("rock", "c8441146"),        
+                "RockDamagePlasticity":  ("rock", "c8441146"),        
+                "RockDamagePlasticityNonLocal":   ("rock", "c8441146"), 
+                "PorousElastic":         ("soil", "c8441146"),        
+                "ModifiedCamClay":       ("soil", "c8441146"),        
+                "Barodesy":              ("soil", "c8441146"), 
+                "SandHypo":              ("soil", "c8441146"), 
+                }
+
+lookUpElementCategories = {  "displacement": "1",
+                              "large displacement": "2",
+                              "gradient-enhanced": "3",
+                              "gradient-enhanced large displacement": "4",
+                              "cosserat": "5",
+                              }
+
+dictElements = { # add element: 
+                  # name: ("displacement/large displacement/gradient-enhanced/ 
+                  #          gradient-enhanced large displacement/cosserat", 
+                # "afbDevelopers/c8441141/c8441146")
+                "uelDisplacement":       ("displacement",       "c8441141"),
+                "uelDisplacementUL":     ("large displacement", "c8441141"),
+                "uelDisplacementTL":     ("large displacement", "c8441141"),
+                "uelNonLocal":           ("gradient-enhanced",  "c8441141"),
+                "uelNonLocalMixed":      ("gradient-enhanced",  "c8441146"),
+                "uelNonLocalEAS":        ("gradient-enhanced",  "c8441141"),
+                "uelNonLocalUL":         ("gradient-enhanced large displacement", "c8441141"),      
+                "uelNonLocalUL":         ("gradient-enhanced large displacement", "c8441141"),        
+                "uelNonLocalULFBar":     ("gradient-enhanced large displacement", "c8441141"), 
+                "uelCosserat":           ("cosserat", "c8441141"),        
+                "uelNonLocalCosserat":   ("cosserat", "c8441141"),      
+                }
+
 
 def cloneGitUibk(projectName,user='afbDevelopers'):
     os.system("git clone git@git.uibk.ac.at:{:}/{:}.git".format(user,projectName))
@@ -37,54 +92,27 @@ def basicInstall(argument):
 def materialInstall(argument):
     os.chdir(os.path.join(cwd,'modules/materials'))
     print("Installing to folder {:}".format(os.path.join(cwd,'modules/materials')))
-    if argument == '1' or 'all':   # basic materials
-        cloneGitUibk("linearElastic") 
-        cloneGitUibk("DruckerPrager","c8441141")
-        cloneGitUibk("MohrCoulomb","c8441146")
-    if argument == '2' or 'all':   # concrete materials
-        cloneGitUibk("CosseratLinearElastic","c8441141")
-        cloneGitUibk("ModLeon","c8441141")  
-        cloneGitUibk("CDP","c8441141")  
-        cloneGitUibk("CDPM2","c8441146")
-    if argument == '3' or 'all':   # shotcrete materials
-        cloneGitUibk("ModLeon","c8441141") 
-        cloneGitUibk("ModLeonNonLocal","c8441141") 
-        cloneGitUibk("ShotLeonV2","c8441141")  
-        cloneGitUibk("SolidificationCreep","c8441141")  
-    if argument == '4' or 'all':   # rock materials
-        cloneGitUibk("HoekBrown","c8441146") 
-        cloneGitUibk("RockDamagePlasticity","c8441146")  
-        cloneGitUibk("RockDamagePlasticityNonLocal","c8441146") 
-    if argument == '5' or 'all':   # rock materials
-        cloneGitUibk("PorousElastic","c8441146") 
-        cloneGitUibk("ModifiedCamClay","c8441146")  
-        cloneGitUibk("Barodesy","c8441146") 
-        cloneGitUibk("SandHypo","c8441146") 
-    else:
-        print("ERROR: No material for your input provided")
+
+    for material, props in dictMaterials.items():
+        category = lookUpMaterialCategories.get(props[0])
+        if argument == 'all':
+            cloneGitUibk(material, props[1])
+        elif category == argument:
+            cloneGitUibk(material, props[1])
+        elif material.lower() == argument.lower():
+            cloneGitUibk(material, props[1])
         
 def elementInstall(argument):
     os.chdir(os.path.join(cwd,'modules/elements'))
     print("Installing to folder {:}".format(os.path.join(cwd,'modules/elements')))
-    if argument == '1' or 'all':   # displacement elements
-        cloneGitUibk("uelDisplacement", "c8441141") 
-    if argument == '2' or 'all':   # large displacement
-        cloneGitUibk("uelDisplacementUL", "c8441141") 
-        cloneGitUibk("uelDisplacementTL", "c8441141") 
-    if argument == '3' or 'all':   # gradient-enhanced 
-        cloneGitUibk("uelNonLocal", "c8441141")
-        cloneGitUibk("uelNonLocalMixed", "c8441141") 
-        cloneGitUibk("uelNonLocalEAS", "c8441141")
-    if argument == '4' or 'all':   # gradient-enhanced largedisplacement 
-        cloneGitUibk("uelNonLocalUL", "c8441141") 
-        cloneGitUibk("uelNonLocalULFBar", "c8441141") 
-    if argument == '5' or 'all':   # Cosserat
-        cloneGitUibk("uelCosserat", "c8441141") 
-        cloneGitUibk("uelNonLocalCosserat", "c8441141") 
-    else:
-        print("ERROR: No element for your input provided")
-    
-    
+    for element, props in dictElements.items():
+        category = lookUpMaterialCategories.get(props[0])
+        if argument == 'all':
+            cloneGitUibk(element, props[1])
+        elif category == argument:
+            cloneGitUibk(element, props[1])
+        elif element.lower() == argument.lower():
+            cloneGitUibk(element, props[1])
     
 if __name__=="__main__":
     
@@ -127,24 +155,31 @@ Input e.g. 1, all, 1 2 3 (hit enter to continue): """)
         if not txt:
             break
         
-        try:
-            for i in userList: basicInstall(i)
-        except:
-            print(" no option chosen")
+        # try:
+        for i in userList: basicInstall(i)
+        # except:
+            # print(" no option chosen")
     
     while True: 
         print("""
 +-----------------------------------------------------------------------------+
 | Installation of materials                                                   |
 +-----------------------------------------------------------------------------+       """)
-        txt = input("""Choose the material libraries you would like to install:
+        print("""Choose the material libraries you would like to install:
+        
+        either as complete packages
             (1) basic materials
             (2) concrete materials
             (3) shotcrete materials
             (4) soil materials
             (5) rock materials
-            
-Input e.g. 1, all, 1 2 3 (hit enter to continue): """)
+        
+        or simply type the name of your desired material from the following list: """)
+        
+        for i in dictMaterials.keys():
+            print("              * {:}".format(i))
+        
+        txt = input("""Input e.g. 1, ModLeon, LinearElastic, all, 1 2 3 (hit enter to continue): """)
         userList = txt.split()
         
         if not txt:
@@ -161,14 +196,19 @@ Input e.g. 1, all, 1 2 3 (hit enter to continue): """)
 +-----------------------------------------------------------------------------+            """)
    
     while True: 
-        txt = input("""Choose the element libraries you would like to install (e.g. 1, 1 2 3, or Enter to quit):
+        print("""Choose the element libraries you would like to install (e.g. 1, 1 2 3, or Enter to quit):
             (1) displacement elements
             (2) large displacement elements (total/updated Lagrangian)
             (3) gradient-enhanced elements
             (4) gradient-enhanced large displacement elements
             (5) Cosserat elements
             
-Input e.g. 1, all, 1 2 3 (hit enter to continue): """)
+        
+        or simply type the name of your desired element from the following list: """)
+        for i in dictElements.keys():
+            print("              * {:}".format(i))
+            
+        txt = input("""Input e.g. 1, uelDisplacement, all, 1 2 3 (hit enter to continue): """)
         userList = txt.split()
         
         if not txt:
