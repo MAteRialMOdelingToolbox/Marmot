4d29ac80b6d0d2b13907a6373ec45e7aa14c9911
Matthias <matthias.neuner@uibk.ac.at>
2020-11-28 17:23:29 +0100

    Renamed Vgt  -> VoigtNotation

--- include/Marmot/DisplacementFiniteElement.h
+++ include/Marmot/DisplacementFiniteElement.h
@@ -303,7 +303,7 @@ void DisplacementFiniteElement< nDim, nNodes >::computeYourself( const double* Q
 
         else if constexpr ( nDim == 2 ) {
 
-            Vector6d dE6 = Vgt::planeVoigtToVoigt( dE );
+            Vector6d dE6 = VoigtNotation::planeVoigtToVoigt( dE );
             Matrix6d C66;
 
             if ( sectionType == SectionType::PlaneStress ) {
@@ -320,7 +320,7 @@ void DisplacementFiniteElement< nDim, nNodes >::computeYourself( const double* Q
                 C = mechanics::getPlaneStrainTangent( C66 );
             }
 
-            S = Vgt::voigtToPlaneVoigt( gaussPt.stress );
+            S = VoigtNotation::voigtToPlaneVoigt( gaussPt.stress );
             gaussPt.strain += dE6;
         }
 
