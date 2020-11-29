01cf384ad4ea2ebb8173a09097f17fa24938bca0
Matthias <matthias.neuner@uibk.ac.at>
2020-11-29 08:36:23 +0100

    Renamed AbaqusUtility -> Utility

---
+++ src/MarmotUtility.cpp
@@ -0,0 +1,15 @@
+#include "Marmot/MarmotUtility.h"
+#include "Marmot/MarmotFunctions.h"
+#include "Marmot/MarmotTypedefs.h"
+#include "Marmot/MarmotVoigt.h"
+
+namespace Marmot {
+
+    void discardTheIncrement( double& pNewDT, double value, const std::string& message )
+    {
+        pNewDT = value;
+        MarmotJournal::warningToMSG( message );
+        return;
+    }
+
+} // namespace Marmot
