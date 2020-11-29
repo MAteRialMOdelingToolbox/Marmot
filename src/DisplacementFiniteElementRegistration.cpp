1f25a6d789338ef3746d0f53fb007eae25fbb481
Matthias <matthias.neuner@uibk.ac.at>
2020-11-29 08:45:07 +0100

    Removed prefix Uel

--- src/DisplacementFiniteElementRegistration.cpp
+++ src/DisplacementFiniteElementRegistration.cpp
@@ -14,56 +14,56 @@ namespace DisplacementFiniteElementRegistration {
     using namespace userLibrary;
     using namespace Marmot::FiniteElement::Quadrature;
 
-    const static bool UelCPS4_isRegistered = MarmotElementFactory::
+    const static bool CPS4_isRegistered = MarmotElementFactory::
         registerElement( "UELCPS4",
-                         userLibrary::ElementCode::UelCPS4,
+                         userLibrary::ElementCode::CPS4,
                          makeFactoryFunction<DisplacementFiniteElement<2, 4>,
                                              FullIntegration,
                                              DisplacementFiniteElement<2, 4>::PlaneStress>() );
 
-    const static bool UelCPE4_isRegistered = MarmotElementFactory::
+    const static bool CPE4_isRegistered = MarmotElementFactory::
         registerElement( "UELCPE4",
-                         userLibrary::ElementCode::UelCPE4,
+                         userLibrary::ElementCode::CPE4,
                          makeFactoryFunction<DisplacementFiniteElement<2, 4>,
                                              FullIntegration,
                                              DisplacementFiniteElement<2, 4>::PlaneStrain>() );
 
-    const static bool UelCPS8R_isRegistered = MarmotElementFactory::
+    const static bool CPS8R_isRegistered = MarmotElementFactory::
         registerElement( "UELCPS8R",
-                         userLibrary::ElementCode::UelCPS8R,
+                         userLibrary::ElementCode::CPS8R,
                          makeFactoryFunction<DisplacementFiniteElement<2, 8>,
                                              ReducedIntegration,
                                              DisplacementFiniteElement<2, 8>::PlaneStress>() );
 
-    const static bool UelCPE8R_isRegistered = MarmotElementFactory::
+    const static bool CPE8R_isRegistered = MarmotElementFactory::
         registerElement( "UELCPE8R",
-                         userLibrary::ElementCode::UelCPE8R,
+                         userLibrary::ElementCode::CPE8R,
                          makeFactoryFunction<DisplacementFiniteElement<2, 8>,
                                              ReducedIntegration,
                                              DisplacementFiniteElement<2, 8>::PlaneStrain>() );
 
-    const static bool UelC3D8_isRegistered = userLibrary::MarmotElementFactory::
-        registerElement( "UELC3D8", userLibrary::ElementCode::UelC3D8, []( int elementID ) -> MarmotElement* {
+    const static bool C3D8_isRegistered = userLibrary::MarmotElementFactory::
+        registerElement( "UELC3D8", userLibrary::ElementCode::C3D8, []( int elementID ) -> MarmotElement* {
             return new DisplacementFiniteElement<3, 8>( elementID,
                                               Marmot::FiniteElement::Quadrature::IntegrationTypes::FullIntegration,
                                               DisplacementFiniteElement<3, 8>::SectionType::Solid );
         } );
 
-    const static bool UelC3D20_isRegistered = userLibrary::MarmotElementFactory::
-        registerElement( "UELC3D20", userLibrary::ElementCode::UelC3D20, []( int elementID ) -> MarmotElement* {
+    const static bool C3D20_isRegistered = userLibrary::MarmotElementFactory::
+        registerElement( "UELC3D20", userLibrary::ElementCode::C3D20, []( int elementID ) -> MarmotElement* {
             return new DisplacementFiniteElement<3, 20>( elementID,
                                                Marmot::FiniteElement::Quadrature::IntegrationTypes::FullIntegration,
                                                DisplacementFiniteElement<3, 20>::SectionType::Solid );
         } );
 
-    const static bool UelC3D20R_isRegistered = userLibrary::MarmotElementFactory::
-        registerElement( "UELC3D20R", userLibrary::ElementCode::UelC3D20R, []( int elementID ) -> MarmotElement* {
+    const static bool C3D20R_isRegistered = userLibrary::MarmotElementFactory::
+        registerElement( "UELC3D20R", userLibrary::ElementCode::C3D20R, []( int elementID ) -> MarmotElement* {
             return new DisplacementFiniteElement<3, 20>( elementID,
                                                Marmot::FiniteElement::Quadrature::IntegrationTypes::ReducedIntegration,
                                                DisplacementFiniteElement<3, 20>::SectionType::Solid );
         } );
 
-    MarmotElement* generateUelT2D2( int elementID )
+    MarmotElement* generateT2D2( int elementID )
     {
         auto uelT2D2 = std::unique_ptr<MarmotElement>(
             new DisplacementFiniteElement<1, 2>( elementID,
@@ -80,7 +80,7 @@ namespace DisplacementFiniteElementRegistration {
                                              std::move( uelT2D2 ) );
     }
     const static bool
-        UelT2D2_isRegistered = userLibrary::MarmotElementFactory::registerElement( "UELT2D2",
-                                                                                userLibrary::ElementCode::UelT2D2,
-                                                                                generateUelT2D2 );
+        T2D2_isRegistered = userLibrary::MarmotElementFactory::registerElement( "UELT2D2",
+                                                                                userLibrary::ElementCode::T2D2,
+                                                                                generateT2D2 );
 } // namespace DisplacementFiniteElementRegistration
