928588a435c106a0b6866611ab2390de2f498a46
Matthias <matthias.neuner@uibk.ac.at>
2020-12-11 17:19:39 +0100

    Added dependecy functionality for CMakeLists.txt

--- module.cmake
+++ module.cmake
@@ -1,3 +1,22 @@
-include_directories(${CMAKE_CURRENT_LIST_DIR}/include)
-file(GLOB sources_element "${CMAKE_CURRENT_LIST_DIR}/src/*.cpp")
-list(APPEND sources ${sources_element})
+set(MODULE_NAME
+    "DisplacementFiniteElement")
+
+set(MODULES_DEPENDENCIES
+    MarmotMechanicsCore
+    MarmotFiniteElementCore 
+    )
+
+set(DEPENDECIES_FULLFILLED TRUE)
+foreach( DEPENDENCY ${MODULES_DEPENDENCIES} )
+    if (NOT DEPENDENCY IN_LIST CORE_MODULES)
+        message("----> " "module ${MODULE_NAME} dependency not fulfilled: ${DEPENDENCY}")
+        set(DEPENDECIES_FULLFILLED FALSE)
+    endif()
+endforeach()
+
+if ( DEPENDECIES_FULLFILLED )
+    set(CMAKE_CXX_STANDARD 17)
+    include_directories(${CMAKE_CURRENT_LIST_DIR}/include)
+    file(GLOB sources_material "${CMAKE_CURRENT_LIST_DIR}/src/*.cpp")
+    list(APPEND sources ${sources_material})
+endif()
