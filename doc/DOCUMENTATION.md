9f302c8dff12820b097ee112ad8170eb222307c6
Dummer Alexander <alexander.dummer@student.uibk.ac.at>
2021-01-07 09:09:20 +0100

    added some documentation content

---
+++ doc/DOCUMENTATION.md
@@ -0,0 +1,23 @@
+## Isotropic Linear Elastic Model based on Hooke's law
+
+\page linearelastic Linear Elastic
+
+## Installation path
+
+ `Marmot/modules/materials/`
+
+## Implementation
+
+The implementation can be found in \ref LinearElastic
+
+## Theory 
+
+The constitutive law is given in total form by
+
+\f[ 
+  \displaystyle \boldsymbol{ \sigma } = \mathbb{ C } : \boldsymbol{ \varepsilon }  
+\f]
+
+relating the nominal stress tensor \f$ \boldsymbol{ \sigma } \f$ 
+to the strain tensor\f$ \boldsymbol{ \varepsilon }  \f$ 
+in terms of the fourth order stiffness tensor \f$ \mathbb{ C } \f$.
--- include/Marmot/LinearElastic.h
+++ include/Marmot/LinearElastic.h
@@ -1,17 +1,54 @@
+/* ---------------------------------------------------------------------
+ *                                       _   
+ *  _ __ ___   __ _ _ __ _ __ ___   ___ | |_ 
+ * | '_ ` _ \ / _` | '__| '_ ` _ \ / _ \| __|
+ * | | | | | | (_| | |  | | | | | | (_) | |_ 
+ * |_| |_| |_|\__,_|_|  |_| |_| |_|\___/ \__|
+ * 
+ * Unit of Strength of Materials and Structural Analysis
+ * University of Innsbruck 
+ * 2020 - today
+ * 
+ * festigkeitslehre@uibk.ac.at
+ * 
+ * 
+ * This file is part of the MAteRialMOdellingToolbox (marmot).
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ * 
+ * The full text of the license can be found in the file LICENSE.md at
+ * the top level directory of marmot.
+ * ---------------------------------------------------------------------
+ */
+
 #pragma once
 #include "Marmot/MarmotMaterialHypoElastic.h"
 #include <iostream>
 #include <string>
-
+/**
+ * \brief Implementation of a linear elastic material 
+ * for 3D stress states.
+ */
 class LinearElastic : public MarmotMaterialHypoElastic {
   public:
     using MarmotMaterialHypoElastic::MarmotMaterialHypoElastic;
 
-    LinearElastic( const double* materialProperties, int nMaterialProperties, int materialNumber );
+    LinearElastic(  const double* materialProperties,
+                    int nMaterialProperties, 
+                    int materialNumber );
 
   protected:
-    const double& E;
+    /// \brief Young's modulus
+    const double& E;    
+    /**< #E represents Young's modulus for isotropic linear elasticity.
+    * It is a reference variable to #materialProperties[0]. */
+    /// \brief Poisson's ratio
     const double& nu;
+    /**< #nu represents Poisson's ratio for isotropic linear elasticity.
+    * It is a reference variable to #materialProperties[1]. */
 
     void computeStress( double* stress,
                         double* dStressDDStrain,
