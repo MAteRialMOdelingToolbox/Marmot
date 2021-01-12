d085ecc4c9d5851d273eb16ae54d6a93b58470d7
Matthias <matthias.neuner@uibk.ac.at>
2021-01-12 12:02:02 +0100

    minor updates

--- module.cmake
+++ module.cmake
@@ -15,7 +15,6 @@ foreach( DEPENDENCY ${MODULES_DEPENDENCIES} )
 endforeach()
 
 if ( DEPENDECIES_FULLFILLED )
-    set(CMAKE_CXX_STANDARD 17)
     include_directories(${CMAKE_CURRENT_LIST_DIR}/include)
     file(GLOB sources_material "${CMAKE_CURRENT_LIST_DIR}/src/*.cpp")
     list(APPEND sources ${sources_material})
