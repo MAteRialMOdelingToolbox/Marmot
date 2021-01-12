15d0dac5f121f0f793aca3670d3a6e95b27754aa
Matthias <matthias.neuner@uibk.ac.at>
2021-01-12 12:40:55 +0100

    added license header

--- MarmotTypedefs.h
+++ MarmotTypedefs.h
@@ -1,3 +1,31 @@
+/* ---------------------------------------------------------------------
+ *                                       _   
+ *  _ __ ___   __ _ _ __ _ __ ___   ___ | |_ 
+ * | '_ ` _ \ / _` | '__| '_ ` _ \ / _ \| __|
+ * | | | | | | (_| | |  | | | | | | (_) | |_ 
+ * |_| |_| |_|\__,_|_|  |_| |_| |_|\___/ \__|
+ * 
+ * Unit of Strength of Materials and Structural Analysis
+ * University of Innsbruck, 
+ * 2020 - today
+ * 
+ * festigkeitslehre@uibk.ac.at
+ * 
+ * Matthias Neuner matthias.neuner@uibk.ac.at
+ * Magdalena Schreter magdalena.schreter@uibk.ac.at
+ * 
+ * This file is part of the MAteRialMOdellingToolbox (marmot).
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ * 
+ * The full text of the license can be found in the file LICENSE.md at
+ * the top level directory of marmot.
+ * ---------------------------------------------------------------------
+ */
+
 #pragma once
 #include "Eigen/Core"
 #include "Eigen/Dense"
