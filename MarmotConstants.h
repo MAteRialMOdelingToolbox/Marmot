8cbb7d03e64627aee05729ea3877903f35695851
Magdalena Karoline Schreter <magdalena.schreter@uibk.ac.at>
2021-01-26 12:30:51 +0100

    Moved plane strain / plane stress functionality to seperate module; Fixed HaighWestergaard includes and namespaces

--- MarmotConstants.h
+++ MarmotConstants.h
@@ -30,17 +30,17 @@
 
 namespace Marmot {
     namespace Constants {
-        const double        Pi         = 3.141592653589793238463;
-        const double        numZeroPos = 1e-16;
+        constexpr double    Pi         = 3.141592653589793238463;
+        constexpr double    numZeroPos = 1e-16;
         double              cubicRootEps();
         double              squareRootEps();
         extern const double CubicRootEps;
         extern const double SquareRootEps;
-        const double        sqrt3_8 = 0.61237243569579452454932101867647;
-        const double        sqrt2_3 = 0.8164965809277260327324280249019;
-        const double        sqrt3_2 = 1.2247448713915890490986420373529;
-        const double        sqrt2   = 1.4142135623730950488016887242097;
-        const double        sqrt3   = 1.7320508075688772935274463415059;
-        const double        sqrt6   = 2.4494897427831780981972840747059;
+        constexpr double    sqrt3_8 = 0.61237243569579452454932101867647;
+        constexpr double    sqrt2_3 = 0.8164965809277260327324280249019;
+        constexpr double    sqrt3_2 = 1.2247448713915890490986420373529;
+        constexpr double    sqrt2   = 1.4142135623730950488016887242097;
+        constexpr double    sqrt3   = 1.7320508075688772935274463415059;
+        constexpr double    sqrt6   = 2.4494897427831780981972840747059;
     } // namespace Constants
 } // namespace Marmot
