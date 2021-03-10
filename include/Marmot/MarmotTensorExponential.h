b0541445b8951c525efcb2e4be4288f752623f76
Matthias <matthias.neuner@uibk.ac.at>
2021-03-10 16:22:17 +0100

    cleanup in functions

--- include/Marmot/MarmotTensorExponential.h
+++ include/Marmot/MarmotTensorExponential.h
@@ -9,7 +9,7 @@ namespace Marmot {
       };
 
       template < typename T, size_t tensorSize >
-      Fastor::Tensor< T, tensorSize, tensorSize > computeTensorExponentialOnly(
+      Fastor::Tensor< T, tensorSize, tensorSize > computeTensorExponential(
         const Fastor::Tensor< T, tensorSize, tensorSize >& theTensor,
         int                                                maxIterations,
         double                                             tolerance,
@@ -55,62 +55,64 @@ namespace Marmot {
         Fastor::Tensor< T, tensorSize, tensorSize, tensorSize, tensorSize > theExponentialDerivative;
       };
 
-      template < typename T, size_t tensorSize >
-      TensorExponentialResult< T, tensorSize > computeTensorExponentialAndDerivate(
-        const Fastor::Tensor< T, tensorSize, tensorSize >& theTensor,
-        int                                                maxIterations,
-        double                                             tolerance )
-      {
-        // tensor exponential;
-        using Tensor = Fastor::Tensor< T, tensorSize, tensorSize >;
-        TensorExponentialResult< T, tensorSize > theResult;
+      namespace FirstOrderDerived {
+        template < typename T, size_t tensorSize >
+        TensorExponentialResult< T, tensorSize > computeTensorExponential(
+          const Fastor::Tensor< T, tensorSize, tensorSize >& theTensor,
+          int                                                maxIterations,
+          double                                             tolerance )
+        {
+          // tensor exponential;
+          using Tensor = Fastor::Tensor< T, tensorSize, tensorSize >;
+          TensorExponentialResult< T, tensorSize > theResult;
 
-        std::vector< Tensor > tensorPowers;
+          std::vector< Tensor > tensorPowers;
 
-        int n;
-        int facN = 1;
+          int n;
+          int facN = 1;
 
-        // term 1 ( n = 0 )
-        theResult.theExponential.eye();
-        tensorPowers.push_back( theResult.theExponential ); // I
+          // term 1 ( n = 0 )
+          theResult.theExponential.eye();
+          tensorPowers.push_back( theResult.theExponential ); // I
 
-        // series 2 ( n = 1 )
-        theResult.theExponential += theTensor;
-        tensorPowers.push_back( theTensor ); // T
+          // series 2 ( n = 1 )
+          theResult.theExponential += theTensor;
+          tensorPowers.push_back( theTensor ); // T
 
-        // loop starts at n = 2
-        for ( n = 2; n < maxIterations; ++n ) {
-          facN *= n;
-          const Tensor tensorPower = tensorPowers.back() % theTensor;
-          const Tensor series      = 1. / facN * tensorPower;
-          theResult.theExponential += series;
+          // loop starts at n = 2
+          for ( n = 2; n < maxIterations; ++n ) {
+            facN *= n;
+            const Tensor tensorPower = tensorPowers.back() % theTensor;
+            const Tensor series      = 1. / facN * tensorPower;
+            theResult.theExponential += series;
 
-          tensorPowers.push_back( tensorPower );
+            tensorPowers.push_back( tensorPower );
 
-          if ( Fastor::norm( series ) <= tolerance )
-            break;
-        }
-
-        // derivative
-        const int N = n;
-
-        facN = 1;
-        Fastor::Tensor< T, tensorSize, tensorSize, tensorSize, tensorSize > innerSeries;
-        Fastor::Tensor< T, tensorSize, tensorSize, tensorSize, tensorSize > outerSeries;
-        outerSeries.zeros();
-        for ( n = 1; n <= N; ++n ) {
-          innerSeries.zeros();
-          for ( int m = 1; m <= n; ++m ) {
-            innerSeries += Fastor::outer( tensorPowers[m - 1], tensorPowers[n - m] ); // ik_lj
+            if ( Fastor::norm( series ) <= tolerance )
+              break;
           }
-          facN *= n;
-          outerSeries += 1. / facN * innerSeries;
-        }
-        theResult.theExponentialDerivative = Fastor::permute< Fastor::Index< 0, 3, 1, 2 > >(
-          outerSeries ); // ik_lj -> ij_kl
 
-        return theResult;
-      }
-    } // namespace TensorExponential
-  }   // namespace ContinuumMechanics::TensorUtility
+          // derivative
+          const int N = n;
+
+          facN = 1;
+          Fastor::Tensor< T, tensorSize, tensorSize, tensorSize, tensorSize > innerSeries;
+          Fastor::Tensor< T, tensorSize, tensorSize, tensorSize, tensorSize > outerSeries;
+          outerSeries.zeros();
+          for ( n = 1; n <= N; ++n ) {
+            innerSeries.zeros();
+            for ( int m = 1; m <= n; ++m ) {
+              innerSeries += Fastor::outer( tensorPowers[m - 1], tensorPowers[n - m] ); // ik_lj
+            }
+            facN *= n;
+            outerSeries += 1. / facN * innerSeries;
+          }
+          theResult.theExponentialDerivative = Fastor::permute< Fastor::Index< 0, 3, 1, 2 > >(
+            outerSeries ); // ik_lj -> ij_kl
+
+          return theResult;
+        }
+      } // namespace FirstOrderDerived
+    }   // namespace TensorExponential
+  }     // namespace ContinuumMechanics::TensorUtility
 } // namespace Marmot
