8e602c102ea59e28f7ab819a192e57878b550eea
Alexander Dummer <alexander.dummer@student.uibk.ac.at>
2021-04-16 13:08:12 +0200

    moved some files to new project from MarmotMechanicsCore

---
+++ include/Marmot/PerezFougetSubstepperMarkII.h
@@ -0,0 +1,155 @@
+/* ---------------------------------------------------------------------
+ *                                       _
+ *  _ __ ___   __ _ _ __ _ __ ___   ___ | |_
+ * | '_ ` _ \ / _` | '__| '_ ` _ \ / _ \| __|
+ * | | | | | | (_| | |  | | | | | | (_) | |_
+ * |_| |_| |_|\__,_|_|  |_| |_| |_|\___/ \__|
+ *
+ * Unit of Strength of Materials and Structural Analysis
+ * University of Innsbruck,
+ * 2020 - today
+ *
+ * festigkeitslehre@uibk.ac.at
+ *
+ * Matthias Neuner matthias.neuner@uibk.ac.at
+ *
+ * This file is part of the MAteRialMOdellingToolbox (marmot).
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * The full text of the license can be found in the file LICENSE.md at
+ * the top level directory of marmot.
+ * ---------------------------------------------------------------------
+ */
+
+#pragma once
+#include "Marmot/MarmotJournal.h"
+#include "Marmot/MarmotMath.h"
+#include "Marmot/MarmotTypedefs.h"
+
+/* Substepper for elastoplastic materials, implicit return mapping version
+ * Matthias Neuner (2016)
+ * */
+
+namespace Marmot::NumericalAlgorithms {
+
+  template < int nSizeMatTangent >
+  class PerezFougetSubstepper {
+
+  public:
+    typedef Eigen::Matrix< double, nSizeMatTangent, nSizeMatTangent > TangentSizedMatrix;
+    PerezFougetSubstepper( double          initialStepSize,
+                           double          minimumStepSize,
+                           double          scaleUpFactor,
+                           double          scaleDownFactor,
+                           int             nPassesToIncrease,
+                           const Matrix6d& Cel );
+    bool   isFinished();
+    double getNextSubstep();
+    bool   decreaseSubstepSize();
+
+    void     finishElasticSubstep();
+    void     finishSubstep( const TangentSizedMatrix& dXdY );
+    Matrix6d consistentStiffness();
+
+  private:
+    const double initialStepSize, minimumStepSize, scaleUpFactor, scaleDownFactor;
+    const int    nPassesToIncrease;
+
+    double currentProgress;
+    double currentSubstepSize;
+    int    passedSubsteps;
+
+    const Matrix6d& Cel;
+
+    TangentSizedMatrix consistentTangent;
+  };
+} // namespace Marmot::NumericalAlgorithms
+
+namespace Marmot::NumericalAlgorithms {
+  template < int n >
+  PerezFougetSubstepper< n >::PerezFougetSubstepper( double          initialStepSize,
+                                                     double          minimumStepSize,
+                                                     double          scaleUpFactor,
+                                                     double          scaleDownFactor,
+                                                     int             nPassesToIncrease,
+                                                     const Matrix6d& Cel )
+    :
+
+      initialStepSize( initialStepSize ),
+      minimumStepSize( minimumStepSize ),
+      scaleUpFactor( scaleUpFactor ),
+      scaleDownFactor( scaleDownFactor ),
+      nPassesToIncrease( nPassesToIncrease ),
+      currentProgress( 0.0 ),
+      currentSubstepSize( initialStepSize ),
+      passedSubsteps( 0 ),
+      Cel( Cel )
+  {
+    consistentTangent = TangentSizedMatrix::Zero();
+  }
+
+  template < int n >
+  bool PerezFougetSubstepper< n >::isFinished()
+  {
+    // this is due to numerical accuracy ...
+    return ( 1.0 - currentProgress ) <= 2e-16;
+  }
+
+  template < int n >
+  double PerezFougetSubstepper< n >::getNextSubstep()
+  {
+    if ( passedSubsteps >= nPassesToIncrease )
+      currentSubstepSize *= scaleUpFactor;
+
+    const double remainingProgress = 1.0 - currentProgress;
+    if ( remainingProgress < currentSubstepSize )
+      currentSubstepSize = remainingProgress;
+
+    passedSubsteps++;
+    currentProgress += currentSubstepSize;
+
+    return currentSubstepSize;
+  }
+
+  template < int n >
+  bool PerezFougetSubstepper< n >::decreaseSubstepSize()
+  {
+    currentProgress -= currentSubstepSize;
+    passedSubsteps = 0;
+
+    currentSubstepSize *= scaleDownFactor;
+
+    if ( currentSubstepSize < minimumStepSize )
+      return MarmotJournal::warningToMSG( "UMAT: Substepper: Minimal stepzsize reached" );
+    else
+      return MarmotJournal::notificationToMSG( "UMAT: Substepper: Decreasing stepsize" );
+  }
+
+  template < int n >
+  void PerezFougetSubstepper< n >::finishElasticSubstep()
+  {
+    consistentTangent += currentSubstepSize * TangentSizedMatrix::Identity();
+  }
+
+  template < int n >
+  void PerezFougetSubstepper< n >::finishSubstep( const TangentSizedMatrix& dXdY )
+  {
+    finishElasticSubstep();
+    consistentTangent.applyOnTheLeft( dXdY );
+
+    for ( int i = 0; i < consistentTangent.rows(); i++ )
+      for ( int j = 0; j < consistentTangent.cols(); j++ )
+        if ( std::abs( consistentTangent( i, j ) ) < 1e-12 )
+          consistentTangent( i, j ) = 0.0;
+  }
+
+  template < int n >
+  Matrix6d PerezFougetSubstepper< n >::consistentStiffness()
+  {
+    return consistentTangent.topLeftCorner( 6, 6 ) * Cel;
+  }
+} // namespace Marmot::NumericalAlgorithms
---
+++ src/MarmotTensor.cpp
@@ -0,0 +1,137 @@
+#include "Marmot/MarmotTensor.h"
+
+using namespace Marmot::ContinuumMechanics::TensorUtility;
+
+namespace Marmot {
+  namespace ContinuumMechanics::CommonTensors {
+
+    auto Initialize_IFourthOrder()
+    {
+      EigenTensors::Tensor3333d I;
+
+      for ( int i = 0; i < 3; i++ )
+        for ( int j = 0; j < 3; j++ )
+          for ( int k = 0; k < 3; k++ )
+            for ( int l = 0; l < 3; l++ ) {
+              I( i, j, k, l ) = d( i, k ) * d( j, l );
+            }
+      return I;
+    }
+    const EigenTensors::Tensor3333d IFourthOrder = Initialize_IFourthOrder();
+
+    auto Initialize_IFourthOrderTranspose()
+    {
+      EigenTensors::Tensor3333d IT;
+
+      for ( int i = 0; i < 3; i++ )
+        for ( int j = 0; j < 3; j++ )
+          for ( int k = 0; k < 3; k++ )
+            for ( int l = 0; l < 3; l++ ) {
+              IT( i, j, k, l ) = d( i, l ) * d( j, k );
+            }
+      return IT;
+    }
+    const EigenTensors::Tensor3333d IFourthOrderTranspose = Initialize_IFourthOrderTranspose();
+
+    auto Initialize_I2xI2()
+    {
+      EigenTensors::Tensor3333d I2xI2;
+
+      for ( int i = 0; i < 3; i++ )
+        for ( int j = 0; j < 3; j++ )
+          for ( int k = 0; k < 3; k++ )
+            for ( int l = 0; l < 3; l++ ) {
+              I2xI2( i, j, k, l ) = d( i, j ) * d( k, l );
+            }
+      return I2xI2;
+    }
+    const EigenTensors::Tensor3333d I2xI2 = Initialize_I2xI2();
+
+    auto Initialize_Isym()
+    {
+      EigenTensors::Tensor3333d Isym;
+
+      for ( int i = 0; i < 3; i++ )
+        for ( int j = 0; j < 3; j++ )
+          for ( int k = 0; k < 3; k++ )
+            for ( int l = 0; l < 3; l++ ) {
+              Isym( i, j, k, l ) = 0.5 * ( d( i, k ) * d( j, l ) + d( i, l ) * d( j, k ) );
+            }
+      return Isym;
+    }
+    const EigenTensors::Tensor3333d Isym = Initialize_Isym();
+
+    auto Initialize_Iskew()
+    {
+      EigenTensors::Tensor3333d Iskew;
+
+      for ( int i = 0; i < 3; i++ )
+        for ( int j = 0; j < 3; j++ )
+          for ( int k = 0; k < 3; k++ )
+            for ( int l = 0; l < 3; l++ ) {
+              Iskew( i, j, k, l ) = 0.5 * ( d( i, k ) * d( j, l ) - d( i, l ) * d( j, k ) );
+            }
+      return Iskew;
+    }
+    const EigenTensors::Tensor3333d Iskew = Initialize_Iskew();
+
+    auto Initialize_dDeviatoricStress_dStress()
+    {
+      EigenTensors::Tensor3333d dsdsigma;
+      dsdsigma.setZero();
+      for ( int i = 0; i < 3; i++ )
+        for ( int j = 0; j < 3; j++ )
+          for ( int k = 0; k < 3; k++ )
+            for ( int l = 0; l < 3; l++ ) {
+              dsdsigma( i, j, k, l ) = d( i, k ) * d( j, l ) - 1. / 3 * d( i, j ) * d( k, l );
+            }
+      return dsdsigma;
+    }
+
+    const EigenTensors::Tensor3333d dDeviatoricStress_dStress = Initialize_dDeviatoricStress_dStress();
+
+    EigenTensors::Tensor333d Initialize_LeviCivita3D()
+    {
+      EigenTensors::Tensor333d e;
+      e.setConstant( 0.0 );
+
+      e( 0, 1, 2 ) = 1.0;
+      e( 1, 2, 0 ) = 1.0;
+      e( 2, 0, 1 ) = 1.0;
+
+      e( 2, 1, 0 ) = -1.0;
+      e( 0, 2, 1 ) = -1.0;
+      e( 1, 0, 2 ) = -1.0;
+
+      return e;
+    }
+
+    EigenTensors::Tensor122d Initialize_LeviCivita2D()
+    {
+      EigenTensors::Tensor122d e;
+      e.setConstant( 0.0 );
+
+      e( 0, 0, 1 ) = 1.0;
+      e( 0, 1, 0 ) = -1.0;
+
+      return e;
+    }
+
+    const EigenTensors::Tensor333d LeviCivita3D = Initialize_LeviCivita3D();
+    const EigenTensors::Tensor122d LeviCivita2D = Initialize_LeviCivita2D();
+
+  } // namespace ContinuumMechanics::CommonTensors
+
+  namespace ContinuumMechanics::TensorUtility {
+    Eigen::Matrix3d dyadicProduct( const Eigen::Vector3d& vector1, const Eigen::Vector3d& vector2 )
+    {
+      Eigen::Matrix3d dyade;
+
+      for ( int i = 0; i < vector1.rows(); i++ )
+        for ( int j = 0; j < vector1.rows(); j++ )
+          dyade( i, j ) = vector1( i ) * vector2( j );
+
+      return dyade;
+    }
+  } // namespace ContinuumMechanics::TensorUtility
+} // namespace Marmot
