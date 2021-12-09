440184f88ac7068ef1676f1ca72bea51450d12bd
Matthias <matthias.neuner@uibk.ac.at>
2021-12-09 19:21:00 +0100

    export of basic definition headers

--- module.cmake
+++ module.cmake
@@ -1,3 +1,8 @@
 include_directories(${CMAKE_CURRENT_LIST_DIR}/include)
 file(GLOB sources_material "${CMAKE_CURRENT_LIST_DIR}/src/*.cpp")
 list(APPEND sources ${sources_material})
+
+list(APPEND publicheaders 
+    "${CMAKE_CURRENT_LIST_DIR}/include/Marmot/MarmotTensor.h" 
+    "${CMAKE_CURRENT_LIST_DIR}/include/Marmot/MarmotTypedefs.h" 
+    )
