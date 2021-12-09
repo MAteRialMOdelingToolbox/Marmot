c6b1ad42f527af1d49ae335ccb220327571e5078
Matthias <matthias.neuner@uibk.ac.at>
2021-12-09 19:21:33 +0100

    export of basic definition headers

--- module.cmake
+++ module.cmake
@@ -7,4 +7,5 @@ list(APPEND publicheaders
     "${CMAKE_CURRENT_LIST_DIR}/include/Marmot/MarmotMaterialHypoElastic.h" 
     "${CMAKE_CURRENT_LIST_DIR}/include/Marmot/MarmotMaterialHyperElastic.h" 
     "${CMAKE_CURRENT_LIST_DIR}/include/Marmot/MarmotMaterialGradientEnhancedMechanical.h" 
+    "${CMAKE_CURRENT_LIST_DIR}/include/Marmot/MarmotVoigt.h" 
     )
