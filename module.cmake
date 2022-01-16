df8a5c0b376e8ce362b18e2224d77bcb56854c3b
Matthias <matthias.neuner@uibk.ac.at>
2022-01-16 23:43:18 +0100

    added header to be install

--- module.cmake
+++ module.cmake
@@ -3,6 +3,8 @@ file(GLOB sources_material "${CMAKE_CURRENT_LIST_DIR}/src/*.cpp")
 list(APPEND sources ${sources_material})
 
 list(APPEND publicheaders 
+    "${CMAKE_CURRENT_LIST_DIR}/include/Marmot/MarmotMath.h" 
+    "${CMAKE_CURRENT_LIST_DIR}/include/Marmot/MarmotConstants.h" 
     "${CMAKE_CURRENT_LIST_DIR}/include/Marmot/MarmotTensor.h" 
     "${CMAKE_CURRENT_LIST_DIR}/include/Marmot/MarmotTypedefs.h" 
     )
