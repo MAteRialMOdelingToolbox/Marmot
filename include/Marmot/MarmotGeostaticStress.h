cac58df12355a648831bff51d6789b40eca70141
Matthias <matthias.neuner@uibk.ac.at>
2022-08-27 23:25:44 +0200

    initial commit

---
+++ include/Marmot/MarmotGeostaticStress.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include "Marmot/MarmotJournal.h"
+#include <Eigen/Core>
+#include <functional>
+
+namespace Marmot::GeostaticStress {
+
+
+std::tuple<double, double, double> getGeostaticStressFromLinearDistribution(const double* geostaticStressDefintion, double coordinate_y);
+
+}
