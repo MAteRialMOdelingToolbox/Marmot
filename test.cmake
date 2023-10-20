23a9c5b1788b1227abc0fedd3ba7e7167eba9e55
Alexander Dummer <alexander.dummer@uibk.ac.at>
2023-10-20 17:53:08 +0200

    fix linking to actually built Marmot lib

--- test.cmake
+++ test.cmake
@@ -1,3 +1,3 @@
 add_executable("LinearElastic_executable" "${CMAKE_CURRENT_LIST_DIR}/test/test.cpp")
-target_link_libraries("LinearElastic_executable" Marmot)
+target_link_libraries("LinearElastic_executable" ${PROJECT_NAME})
 add_test(NAME "LinearElastic" COMMAND "LinearElastic_executable")
