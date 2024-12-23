6cc900f564abb3b224ceb55fe7d1075d0704a24a
Nasser Alkmim <nasser.alkmim@gmail.com>
2024-12-23 12:51:31 +0100

    Add test for MarmotStateVarVectorManager

--- modules/core/MarmotMechanicsCore/test/TestMarmotStateVarVectorManager.cpp
+++ modules/core/MarmotMechanicsCore/test/TestMarmotStateVarVectorManager.cpp
@@ -1,19 +1,39 @@
+#include "Marmot/MarmotStateVarVectorManager.h"
 #include "Marmot/MarmotTesting.h"
-
-/*
- * ASSIGNEE: Nasser Alkmim
- * TODO: Test MarMotStateVarVectorManager implementation
- */
 
 using namespace Marmot::Testing;
 
-void myDummyTestFunction()
+// Create dummy manager for testing (based on VonMises.h)
+class DummyMaterialStateVarManager : public MarmotStateVarVectorManager {
+public:
+  inline const static auto layout = makeLayout( { { .name = "var1", .length = 3 }, { .name = "var2", .length = 2 } } );
+
+  // Constructor initializes the parent class
+  DummyMaterialStateVarManager( double* stateVars ) : MarmotStateVarVectorManager( stateVars, layout ) {};
+};
+
+void testStateVarVectorManagerFind()
 {
-  throwExceptionOnFailure( checkIfEqual( 1, 1 ), "1 == 1" );
+  DummyMaterialStateVarManager manager( new double[5] );
+
+  double& var1 = manager.find( "var1" );
+  var1         = 1.0;
+  throwExceptionOnFailure( checkIfEqual( 1, var1 ), "1 == 1" );
+}
+
+void testStateVarVectorManagerContains()
+{
+  DummyMaterialStateVarManager manager( new double[5] );
+
+  throwExceptionOnFailure( manager.contains( "var1" ), "var1 should be contained" );
+  throwExceptionOnFailure( manager.contains( "var2" ), "var2 should be contained" );
+  throwExceptionOnFailure( !manager.contains( "var3" ), "var3 should not be contained" );
 }
 
 int main()
 {
-  myDummyTestFunction();
+  testStateVarVectorManagerFind();
+  testStateVarVectorManagerContains();
+
   return 0;
 }
