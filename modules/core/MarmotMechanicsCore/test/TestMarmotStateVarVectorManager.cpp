ffd889d887287e368708601e24780d917c6e9831
Nasser Alkmim <nasser.alkmim@gmail.com>
2025-04-10 15:30:37 +0200

    refact: update test

--- modules/core/MarmotMechanicsCore/test/TestMarmotStateVarVectorManager.cpp
+++ modules/core/MarmotMechanicsCore/test/TestMarmotStateVarVectorManager.cpp
@@ -4,9 +4,12 @@
 using namespace Marmot::Testing;
 
 // Create dummy manager for testing (based on VonMises.h)
+// Base class is protected, so we need to create a derived class
 class DummyMaterialStateVarManager : public MarmotStateVarVectorManager {
 public:
-  inline const static auto layout = makeLayout( { { .name = "var1", .length = 3 }, { .name = "var2", .length = 2 } } );
+  inline const static auto layout = makeLayout( {
+    { .name = "var1", .length = 3 },
+    { .name = "var2", .length = 2 } } );
 
   // Constructor initializes the parent class
   DummyMaterialStateVarManager( double* stateVars ) : MarmotStateVarVectorManager( stateVars, layout ) {};
@@ -14,20 +17,25 @@ public:
 
 void testStateVarVectorManagerFind()
 {
-  DummyMaterialStateVarManager manager( new double[5] );
+  std::vector<double> stateVarData(5);
+  DummyMaterialStateVarManager manager( stateVarData.data() );
 
   double& var1 = manager.find( "var1" );
-  var1         = 1.0;
-  throwExceptionOnFailure( checkIfEqual( 1, var1 ), "1 == 1" );
+  throwExceptionOnFailure( checkIfEqual( var1, 0.0 ), "Initial value of var1" );
+  throwExceptionOnFailure(&var1 == stateVarData.data(), "var1 should point to the first element of stateVarData" );
+  double& var2 = manager.find( "var2" );
+  throwExceptionOnFailure(&var2 == stateVarData.data() + 3, "var2 should point to the fourth element of stateVarData" );
 }
 
 void testStateVarVectorManagerContains()
 {
-  DummyMaterialStateVarManager manager( new double[5] );
+  std::vector<double> stateVarData(5);
+  DummyMaterialStateVarManager manager( stateVarData.data() );
 
   throwExceptionOnFailure( manager.contains( "var1" ), "var1 should be contained" );
   throwExceptionOnFailure( manager.contains( "var2" ), "var2 should be contained" );
   throwExceptionOnFailure( !manager.contains( "var3" ), "var3 should not be contained" );
+  throwExceptionOnFailure( !manager.contains( "" ), "Empty string should not be contained" );
 }
 
 int main()
