c34b4906853e4a11a070627a6a97c80ec19df7ca
TheMagnificentM <manuel.hradsky@uibk.ac.at>
2025-10-12 04:25:54 +0200

    doc: LinerarElastic and ADLinearElastic (#7)
    * documentation changes 250904
    
    * 250915 - changes in documentation
    
    * docs: add documentation for LinearElastic and ADLinearElastic

--- modules/materials/LinearElastic/include/Marmot/LinearElastic.h
+++ modules/materials/LinearElastic/include/Marmot/LinearElastic.h
@@ -33,7 +33,7 @@
 
 namespace Marmot::Materials {
   /**
-   * \brief Implementation of a linear elastic material
+   * @brief Implementation of a linear elastic material
    * for 3D stress states.
    *
    * For further information see \ref linearelastic.
@@ -45,110 +45,111 @@ namespace Marmot::Materials {
     LinearElastic( const double* materialProperties, int nMaterialProperties, int materialNumber );
 
   protected:
-    /// \brief Type of anisotropic behavior.
+    /// @brief Type of isotropic and anisotropic behavior.
     /** #Type is an enum class which involves the following case:*/
 
     enum class Type {
       Isotropic           = 2,  /**< Number of materialProperties equals 2.*/
-      TransverseIsotropic = 11, /**< Number of materialProperties equals 8. 5 to describe
-                                 *   the material behavior and 3 components of a normal
-                                 *   vector. The latter corresponds to the x1 - axis of the principal material
-                                 *   directions and defines the materials plane of isotropy (directions x2 and x3).
+      TransverseIsotropic = 11, /**< Number of materialProperties equals 11.
+                                 *   5 parameters describe the material behavior and
+                                 *   6 parameters correspond to two normal vectors.
+                                 *   The first vector gives the anisotropic direction (direction \f$x_1\f$).
+                                 *   The second vector (direction \f$x_2\f$) is the first of the two needed vectors for
+                                 * the isotropic plane (directions \f$x_2\f$ and \f$x_3\f$). The \f$x_3\f$ axis is
+                                 * obtained as the normalized cross product of the two vectors.
                                  */
-      Orthotropic = 15          /**< Number of materialProperties equals 12. 9 to describe material behavior and
-                                 * 	3 components of a orthogonal vectors. The latter corresponds to the
-                                 *   x1 and x2 of the principal material directions.
+      Orthotropic = 15          /**< Number of materialProperties equals 15.
+                                 * 	 9 parameters describe the material behaviour and
+                                 *   6 parameters are the components of two vectors that define the principal material
+                                 * directions.          The two vectors correspond to the directions \f$x_1\f$ and \f$x_2\f$.          The
+                                 * third          principal axis \f$x_3\f$ is the unit normal to the plane spanned by those
+                                 * vectors (e.g.          the          normalized cross product).
                                  */
     } anisotropicType;          /**< #anisotropicType represents the directional dependence of the material behavior.
                                  *  	It is a representative of the enum class #Type. */
 
-    /// \brief Young's modulus in x1 - direction
-    /** #E1 represents the Young's modulus effective in the direction of the x1 - axis
-     * of the user defined local coordinate system.
-     * It is a reference variable to #materialProperties[0]. */
-
+    /// @brief Young's modulus in \f$x_1\f$-direction
+    /** \f$E_1\f$ represents the Young's modulus effective in the direction of the \f$x_1\f$-axis of the user-defined
+     * local coordinate system. It is a reference variable to #materialProperties[0].
+     */
     const double& E1;
 
-    /// \brief Young's modulus in x2 - direction
-    /** #E2 represents the Young's modulus effective in the direction of the x2 - axis
-     * of a user defined local coordinate system. In case of isotropic behavior it is
-     * set to #E1 and does not need to be specified.
-     * Otherwise it is a reference variable to #materialProperties[1]. */
-
+    /// @brief Young's modulus in \f$x_2\f$-direction
+    /** \f$E_2\f$ represents the Young's modulus effective in the direction of the \f$x_2\f$-axis of the user-defined
+     * local coordinate system. In case of isotropic behavior it is set to \f$E_1\f$ and does not need to be specified.
+     * Otherwise it is a reference variable to #materialProperties[1].
+     */
     const double& E2;
 
-    /// \brief Young's modulus in x3 - direction
-    /** #E3 represents the Young's modulus effective in the direction of the x3 - axis
-     * of a user defined local coordinate system. In case of orthotropic behavior it needs
-     * to be specified and is a reference variable to #materialProperties[2].
-     *
-     * In case of transverse isotropic and isotropic behavior it is automatically set
-     * to #E2 and does not need to be specified. */
-
+    /// @brief Young's modulus in \f$x_3\f$-direction
+    /** \f$E_3\f$ represents the Young's modulus effective in the direction of the \f$x_3\f$-axis of the user-defined
+     * local coordinate system.
+     * - For orthotropic behavior it must be specified and is a reference variable
+     *   to #materialProperties[2].
+     * - For transverse-isotropic or isotropic behavior it is automatically set
+     *   to \f$E_2\f$ and does not need to be specified.
+     */
     const double& E3;
 
-    /// \brief Poisson's ratio between axis x1 and x2.
-    /** #nu12 represents the Poisson's ratio effective between the x1 and x2 - axis of
-     * the user defined local coordinate system. Depending on the anisotropic type it is a
-     * reference variable to:
-     * 	- Isotropic:		#materialProperties[1]
-     *  	- TransverseIsotropic:	#materialProperties[2]
-     *  	- Orthotropic:		#materialProperties[3]. */
-
+    /// @brief Poisson's ratio between axes \f$x_1\f$ and \f$x_2\f$
+    /** \details \f$\nu_{12}\f$ represents the Poisson's ratio describing the strain in direction \f$x_1\f$ caused by a
+     * load in direction \f$x_2\f$ of the user-defined local coordinate system. Depending on the anisotropic type it is
+     * a reference variable to:
+     *   - Isotropic:           #materialProperties[1]
+     *   - TransverseIsotropic: #materialProperties[2]
+     *   - Orthotropic:         #materialProperties[3]
+     */
     const double& nu12;
 
-    /// \brief Poisson's ratio between axis x2 and x3.
-    /** #nu23 represents the Poisson's ratio effective between the x2 and x3 - axis of
-     * the user defined local coordinate system. Depending on the anisotropic type it is a
-     * reference variable to:
-     *  	- TransverseIsotropic:	#materialProperties[3]
-     *  	- Orthotropic:		#materialProperties[4].
-     *
-     * In case of isotropic behavior it is set to #nu12 and does not need to be specified. */
-
+    /// @brief Poisson's ratio between axes \f$x_2\f$ and \f$x_3\f$
+    /** \details \f$\nu_{23}\f$ represents the Poisson's ratio describing the strain in direction \f$x_2\f$ caused by a
+     * load in direction \f$x_3\f$ of the user-defined local coordinate system. Depending on the anisotropic type it is
+     * a reference variable to:
+     *   - TransverseIsotropic: #materialProperties[3]
+     *   - Orthotropic:         #materialProperties[4]
+     * For isotropic behaviour it is set to #nu12 and does not need to be specified.
+     */
     const double& nu23;
 
-    /// \brief Poisson's ratio between axis x1 and x3.
-    /** #nu13 represents the Poisson's ratio effective between the x1 and x3 - axis of
-     * the user defined local coordinate system. In case of orthotropic behavior
-     * it needs to be specified and is a reference variable to #materialProperties[5].
-     *
-     * In case of transverse isotropic and isotropic behavior it is set to #nu23 and does not need to be
-     * specified.*/
-
+    /// @brief Poisson's ratio between axes \f$x_1\f$ and \f$x_3\f$
+    /** \details \f$\nu_{13}\f$ represents the Poisson's ratio describing the strain in direction \f$x_1\f$ caused by a
+     * load in direction \f$x_3\f$ of the user-defined local coordinate system.
+     * - For orthotropic behaviour it must be specified (#materialProperties[5]).
+     * - For transverse-isotropic and isotropic behaviour it is set to #nu23 and does not need to be specified.
+     */
     const double& nu13;
 
-    /// \brief Shear modulus between axis x1 and x2.
-    /** #G12 represents the Shear modulus effective between the x1 and x2 - axis of the user defined
-     * local coordinate system. In case of isotropic behavior, it does not need to be specified and is
-     * set to \f$ \frac{E_1}{2 \cdot ( 1 + \nu_{12})}\f$.
-     * Otherwise, depending on the anisotropic type it is a reference variable to:
-     *  	- TransverseIsotropic:	#materialProperties[4].
-     *  	- Orthotropic:		#materialProperties[6].*/
-
+    /// @brief Shear modulus between axes \f$x_1\f$ and \f$x_2\f$
+    /** \details \f$G_{12}\f$ represents the shear modulus effective between the \f$x_1\f$- and \f$x_2\f$-axes of the
+     * user-defined local coordinate system. In case of isotropic behavior it does not need to be specified and is set
+     * to \f$\tfrac{E_1}{2 \cdot (1 + \nu_{12})}\f$. Otherwise, depending on the anisotropic type it is a reference
+     * variable to:
+     *   - TransverseIsotropic: #materialProperties[4]
+     *   - Orthotropic:         #materialProperties[6]
+     */
     const double G12;
 
-    /// \brief Shear modulus between axis x2 and x3.
-    /** #G23 represents the Shear modulus effective between the x2 and x3 - axis of the user defined
-     * local coordinate system. Neither isotropic nor transversal isotropic behavior needs to be specified.
-     * In case of isotropic behavior it is set to #G12 and in case of transverse isotropic behavior
-     * it is set to \f$ \frac{E_2}{2 \cdot ( 1 + \nu_{23})}\f$
-     * In case of orthotropic behavior it is a reference variable to #materialProperties[7].*/
-
+    /// @brief Shear modulus between axes \f$x_2\f$ and \f$x_3\f$
+    /** \details \f$G_{23}\f$ represents the shear modulus effective between the \f$x_2\f$- and \f$x_3\f$-axes of the
+     * user-defined local coordinate system. For isotropic behavior it is set to #G12. For transverse-isotropic behavior
+     * it is set to \f$\tfrac{E_2}{2 \cdot (1 + \nu_{23})}\f$. For orthotropic behavior it is a reference variable to
+     * #materialProperties[7].
+     */
     const double G23;
 
-    /// \brief Shear modulus between axis x1 and x3.
-    /** #G13 represents the Shear modulus effective between the x1 and x3 - axis of the user defined
-     * local coordinate system. Neither isotropic nor transversal isotropic behavior needs to be specified and it
-     * is set in both cases to #G12.
-     * In case of orthotropic behavior it is a reference variable to #materialProperties[8].*/
-
+    /// @brief Shear modulus between axes \f$x_1\f$ and \f$x_3\f$
+    /** \details \f$G_{13}\f$ represents the shear modulus effective between the \f$x_1\f$- and \f$x_3\f$-axes of the
+     * user-defined local coordinate system.
+     * - For isotropic and transverse-isotropic behavior it is set to #G12.
+     * - For orthotropic behavior it is a reference variable to #materialProperties[8].
+     */
     const double& G13;
 
-    /// \brief Material stiffness tensor.
+    /// @brief Material stiffness tensor.
     /** #globalStiffnessTensor represents the materials stiffness tensor in voigt notation
      * in the global coordinate system.
-     * It is calculated by the functions implemented in \ref MarmotElasticity.h .*/
+     * It is calculated by the functions implemented in *MarmotElasticity.h*.
+     */
 
     Matrix6d globalStiffnessTensor;
 
