cf983d0963736cb50ea61f92bacf2b9ec19c05cc
Alexander Dummer <62308023+alexdummer@users.noreply.github.com>
2025-10-22 11:07:10 -0600

    add factorial to header

--- modules/core/MarmotMathCore/include/Marmot/MarmotMath.h
+++ modules/core/MarmotMathCore/include/Marmot/MarmotMath.h
@@ -73,6 +73,14 @@ namespace Marmot {
      * the maximum limit is returned. If \ref x is smaller than the minimum limit, the minimum limit is returned.
      */
     double exp( double x );
+    
+
+    /** 
+     * @brief Runtime factorial
+     * @param n An unsigned integer
+     * @return The factorial of n 
+     */    
+    unsigned long factorial( unsigned int n );
 
     /** @brief Extracts the exponent to the power of ten from a floating point number
      *  @param x Input floating point number
