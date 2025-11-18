da8982b08a23b57c24b2c9bbbe0d207d974f4d29
Alexander Dummer <alexander.dummer@uibk.ac.at>
2025-11-18 16:40:57 -0700

    add necessary public headers

--- modules/core/MarmotFiniteElementCore/module.cmake
+++ modules/core/MarmotFiniteElementCore/module.cmake
@@ -2,5 +2,7 @@ include_directories(${CMAKE_CURRENT_LIST_DIR}/include)
 file(GLOB sources_material "${CMAKE_CURRENT_LIST_DIR}/src/*.cpp")
 list(APPEND sources ${sources_material})
 list(APPEND publicheaders
+    "${CMAKE_CURRENT_LIST_DIR}/include/Marmot/MarmotElement.h"
     "${CMAKE_CURRENT_LIST_DIR}/include/Marmot/MarmotElementFactory.h"
+    "${CMAKE_CURRENT_LIST_DIR}/include/Marmot/MarmotElementProperty.h"
     )
--- modules/core/MarmotUtilitiesCore/module.cmake
+++ modules/core/MarmotUtilitiesCore/module.cmake
@@ -1,3 +1,7 @@
 include_directories(${CMAKE_CURRENT_LIST_DIR}/include)
 file(GLOB sources_material "${CMAKE_CURRENT_LIST_DIR}/src/*.cpp")
 list(APPEND sources ${sources_material})
+list(APPEND publicheaders
+    "${CMAKE_CURRENT_LIST_DIR}/include/Marmot/MarmotStateHelpers.h"
+    "${CMAKE_CURRENT_LIST_DIR}/include/Marmot/MarmotUtils.h"
+    )
